cda8142e71efb39ef0f949c5682b62927206a513
moein-71 <mynbzrgy4@gmail.com>
2025-03-11 03:47:18 +0330

    Calendar Event : start git status

---
+++ CalendarEvent/include/event.hpp
@@ -0,0 +1,19 @@
+#ifndef CALENDER_IG
+#define CALENDER_IG
+
+#include <iostream>
+
+class Event {
+
+    public :
+
+    private :
+        std::string event_name ;
+        int start_time ;
+        int end_time ;
+
+} ;
+
+
+
+#endif //CALENDER_IG
