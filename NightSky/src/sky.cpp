c17810c850d9e7bf06519a892e7ee24b8c9f9050
moein-71 <mynbzrgy4@gmail.com>
2025-03-18 12:59:45 +0330

    Night sky : sky source file added

---
+++ NightSky/src/sky.cpp
@@ -0,0 +1,37 @@
+#include "sky.hpp"
+
+using namespace std ;
+
+Sky::Sky() 
+{
+    star_list.reserve(10) ;
+}
+
+void Sky::add_star(Star new_star) 
+{
+    star_list.push_back(new_star) ;
+}
+
+void Sky::draw() 
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth ,screenHeight , "Night_sky") ;
+
+    SetTargetFPS(60) ;
+
+    while(!WindowShouldClose()) 
+    {
+        BeginDrawing() ;
+
+        ClearBackground(BLACK) ;
+
+        for(const Star & temp_star : star_list) 
+        {
+            DrawCircle(temp_star.get_x() , temp_star.get_y() , temp_star.get_brightness() , WHITE) ;
+        }
+
+        EndDrawing() ;
+    }
+}
